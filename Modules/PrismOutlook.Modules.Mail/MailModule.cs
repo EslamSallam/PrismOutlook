2bcb2bee78da8ad15d9177e9dcaa93f4f8e0e3bd
EslamSallam <eslam.salam1369@mail.com>
2022-07-14 09:40:33 +0200

    Add project files.

---
+++ Modules/PrismOutlook.Modules.Mail/MailModule.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using PrismOutlook.Core;
+using PrismOutlook.Modules.Mail.Views;
+
+namespace PrismOutlook.Modules.Mail
+{
+    public class MailModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public MailModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //TODO: remove 
+            regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ PrismOutlook/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using PrismOutlook.Modules.Mail;
+using PrismOutlook.Core;
+using PrismOutlook.Views;
+using System.Windows;
+using Prism.Regions;
+using Infragistics.Windows.OutlookBar;
+using PrismOutlook.Core.Regions;
+using Fluent;
+
+namespace PrismOutlook
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<MailModule>();
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(XamOutlookBar), Container.Resolve<XamOutlookGroupRegionAdapter>() );
+            regionAdapterMappings.RegisterMapping(typeof(Ribbon), Container.Resolve<RibbonRegionAdapter>() );
+        }
+    }
+}
